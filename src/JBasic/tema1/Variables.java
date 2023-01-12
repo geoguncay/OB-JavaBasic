f7db12fd68a118439501682916e43c1b4e51f606
geo <geo@email.com>
2023-01-11 19:51:01 -0500

    tema 1

---
+++ src/JBasic/tema1/Variables.java
@@ -0,0 +1,12 @@
+package JBasic.tema1;
+
+public class Variables {
+  public static void main(String[] args) {
+    int intN = 5; // int variable type
+    double doubleN = 5.0; // double variable type
+    float floatN = 5.0f; // float variable type
+    char charT = 'a'; // char variable type
+    boolean booleanT = true; // boolean variable type
+  }
+
+}
