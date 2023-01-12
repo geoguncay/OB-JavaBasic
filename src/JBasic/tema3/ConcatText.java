2fd0e895c27b1426477a88f14994f4a36f56a913
geo <geo@email.com>
2023-01-11 21:01:27 -0500

    tema 3

---
+++ src/JBasic/tema3/ConcatText.java
@@ -0,0 +1,11 @@
+package JBasic.tema3;
+
+public class ConcatText {
+  public static void main(String[] args) {
+    String [] textName = {"Juan", "Jhon", "Pedro"};
+    
+    for ( String i : textName ) {
+      System.out.println(i);
+    }
+  }
+}
